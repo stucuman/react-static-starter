a22c19f947e7829e1caf45a295d314d0985eddd5
Seba Gomes <sebastian.gomes@gmail.com>
2021-05-26 13:53:21 -0300

    Update Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,5 +6,64 @@ export default () => (
   <div>
     <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
     <img src={logoImg} alt="" />
+
+Las <a href="http://lacritica.com.ar">noticias de Tucuman</a>, estan informadas dia a dia por diversos medios online, algunos tradicionales como La Gaceta y <a href="http://lacritica.com.ar">Contexto Tucuman</a>. Otros mas nuevos como ser <a href="http://lacritica.com.ar">Los primeros</a> y el tucumano, que ya gozan de una muy buena cantidad de visitas.
+
+Los Diarios de Tucuman muestran noticias de todo el ámbito de la provincia, información deportiva como así también las novedades en torno a la inseguridad.
+
+Vas a encontrar información sobre
+<h3>inseguridad en tucuman</h3>
+<h3><a href="http://lacritica.com.ar">noticias de tucuman</a></h3>
+<h3><a href="https://lacritica.com.ar/clima-en-tucuman/">Clima en Tucuman</a></h3>
+<h3>ceferino decima</h3>
+<h3><a href="https://lacritica.com.ar/2020/02/03/ana-pedraza-la-periodista-tucumana-que-cautiva-con-su-carisma-y-aman-en-buenos-aires/"> ana pedraza</a></h3>
+<h3>Celia Nahra</h3>
+<h3>juan manzur</h3>
+<h3>osvaldo jaldo</h3>
+<h3>jose cano</h3>
+<h3>silvia elias de perez</h3>
+<h3>jose maria canelada</h3>
+<h3><a href="https://lacritica.com.ar/clima-en-tucuman/">clima en Tucuman</a></h3>
+<h3><a href="https://lacritica.com.ar/quiniela-de-tucuman"><strong>quiniela de tucuman</strong></a></h3>
+<h3><a href="https://lacritica.com.ar/2020/04/27/es-la-senora-poron-el-tucu-lopez-mostro-de-mas-y-ardio-twitter/">tucu lopez</a></h3>
+<h3><a class="" href="https://lacritica.com.ar/2020/01/27/silvina-saleme-posleman-le-prendio-fuego-a-los-cerros-tucumanos-con-fotos-super-hot/">silvina saleme posleman</a></h3>
+<h3><a href="https://lacritica.com.ar/2020/02/19/jorgelina-zalazar-vs-gladys-la-bomba-tucumana-que-susto-que-se-llevo-la-video/?swcfpc=1">jorgelina zalazar</a></h3>
+<h3>pablo verxelitaki</h3>
+<h3>daniela suarez</h3>
+<h3><a href="https://lacritica.com.ar/?s=daniela+orce%C2%A0">daniela orce</a></h3>
+<h3><a href="https://lacritica.com.ar/?s=lucia+rossi%C2%A0">lucia rossi</a></h3>
+<h3><a href="http://argencon.org">Argencon</a></h3>
+<h3><a href="http://telefonosatencioncliente.xyz">Telefonos de atencion al cliente</a></h3>
+<h3><a href="https://lacritica.netlify.app">netlify</a></h3>
+<h3><a href="https://sites.google.com/view/tuquman/">tucuman</h3>
+<h3><a href="https://sebastiangomes5.wixsite.com/tuquman">tucuman</h3>
+<h3><a href="https://proyectoasistir.com.ar">piscologo online</a></h3>
+<h3><a href="https://sastreriagiordano.com.ar">alquiler de trajes en tucuman</a></h3>
+<h3><a href="https://proyectoasistir.com.ar">piscologo online </a></h3>
+<h3><a href="https://sastreriagiordano.com.ar">alquiler de trajes para fiestas en tucuman</a></h3>
+<h3><a href="https://sastreriagiordano.com.ar">alquiler de trajes en tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">picadas en Tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">delivery de picadas en Tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">picadas a domicilio en Tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">picadas para regalar en Tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">regalos empresariales en Tucuman</a></h3>
+<h3><a href="https://quepicada.com.ar">box para regalar en tucuman</a></h3>
+<h3><a href="https://images.google.de/url?q=https://lacritica.com.ar/">noticias de tucuman</a>
+<h3><a href="https://images.google.co.uk/url?q=https://lacritica.com.ar/">noticias de tucuman</a>
+<h3><a href="https://images.google.co.jp/url?q=https://lacritica.com.ar/">tucuman</a>
+<h3><a href="https://images.google.co.jp/url?q=https://proyectoasistir.com.ar">piscologo online</a></h3>
+<h3><a href="https://images.google.es/url?q=https://lacritica.com.ar/">ultimo momento tucuman</a>
+<h3><a href="https://www.ted.com/profiles/28003350">TED</h3>
+<h3><a href="https://people.mozilla.org/p/sebasg">Mozilla</h3>
+<h3><a href="https://www.google.es/url?q=https://lacritica.com.ar/"> ricardo bussi </a></h3>
+<h3><a href="https://spark.adobe.com/page/QGyPFWsmiJCTK/">adobe </a></h3>
+<h3><a href="marketingdigitalexperience.com/curso-de-marketing-digital/">curso de marketing digital </a></h3>
+<h3><a href="https://huffduffer.com/sebasg/610279">huffduffer</h3>
+<h3><a href="https://spacebabe.ams3.digitaloceanspaces.com/index.html">Spaces babe</a></h3>
+<h3><a href="https://epaepa.oss-ap-northeast-1.aliyuncs.com/index.html">Epaepadiscoestepa</a></h3>
+<h3><a href="https://bit.ly/3eLLr0v">bitly</a></h3>
+<h3><a href="https://tucuman.hatenablog.com/entry/2021/05/15/133138">hatena</a></h3>
+<h3><a href="https://lacritica.com.ar/2021/05/16/barbie-beltramino/">Barbie Beltramino</a></h3>
+<h3><a href="https://ar.pinterest.com/LACRITICAok">pinterest</a></h3>
   </div>
 )
